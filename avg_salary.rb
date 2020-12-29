d67b85e75488b106e6c8c898260ea4d80c12f644
romigos <bolistovskiyroman@gmail.com>
2020-12-29 17:49:31 +0200

    first task some fix

--- avg_salary.rb
+++ avg_salary.rb
@@ -1,13 +1,23 @@
 class Company
-  data = [{:fio => "Іванов ІІ", :position => "економіст", :department => "НБО", :salary => "1000"},
-          {:fio => "Петров ПП", :position => "економіст", :department => "НБО", :salary => "1200"},
-          {:fio => "Сідоров ВВ", :position => "економіст", :department => "НБО", :salary => "4500"},
-          {:fio => "Козлов СС", :position => "економіст", :department => "НБО", :salary => "7111"}]
+  def initialize(fio, position, department, sallary)
+    @company_fio = fio
+    @company_position = position
+    @company_department = department
+    @company_sallary = sallary
+  end
 
   def avg_salary
-    puts average = data { :salary }.sum / data { :salary }.size.to_f
+    puts @company_fio, @company_position, @company_department, @company_sallary
   end
 end
+
+
+c = Company.new({:fio => "Іванов ІІ", :position => "економіст", :department => "НБО", :salary => "1000"},
+                {:fio => "Петров ПП", :position => "економіст", :department => "НБО", :salary => "1200"},
+                {:fio => "Сідоров ВВ", :position => "економіст", :department => "НБО", :salary => "4500"},
+                {:fio => "Козлов СС", :position => "економіст", :department => "НБО", :salary => "7111"})
+c.avg_salary
+
 
 
 
